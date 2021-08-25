5b8f3feb0ddee15cf584984cf000d6eda73d5358
Ben Kasper <bckasper@gmail.com>
2021-08-25 16:20:44 -0400

    added IDs to each of the buttons (ended up not using in functionality though). Also, sent the user inputs to local storage. Working on extracting those stored inputs back to the DOM and began attempting to create that function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,43 +1,53 @@
 // Document variables
 var currentDay = $('#currentDay')
+var saveBtns = $('button')
 
 // Created an array of all the <input> timeblocks so I can loop through and compare to the current hour and apply classes
 var timeBlock = [
     {
         "element": $('#timeblock-1'),
-        "hour": $('#timeblock-1').attr("data-hour")
+        "hour": $('#timeblock-1').attr("data-hour"),
+        "value": $('#timeblock-1').text()
     },
     {
         "element": $('#timeblock-2'),
-        "hour": $('#timeblock-2').attr("data-hour")
+        "hour": $('#timeblock-2').attr("data-hour"),
+        "value": $('#timeblock-2').val()
     },
     {
         "element": $('#timeblock-3'),
-        "hour": $('#timeblock-3').attr("data-hour")
+        "hour": $('#timeblock-3').attr("data-hour"),
+        "value": $('#timeblock-3').val()
     },
     {
         "element": $('#timeblock-4'),
-        "hour": $('#timeblock-4').attr("data-hour")
+        "hour": $('#timeblock-4').attr("data-hour"),
+        "value": $('#timeblock-4').val()
     },
     {
         "element": $('#timeblock-5'),
-        "hour": $('#timeblock-5').attr("data-hour")
+        "hour": $('#timeblock-5').attr("data-hour"),
+        "value": $('#timeblock-5').val()
     },
     {
         "element": $('#timeblock-6'),
-        "hour": $('#timeblock-6').attr("data-hour")
+        "hour": $('#timeblock-6').attr("data-hour"),
+        "value": $('#timeblock-6').val()
     },
     {
         "element": $('#timeblock-7'),
-        "hour": $('#timeblock-7').attr("data-hour")
+        "hour": $('#timeblock-7').attr("data-hour"),
+        "value": $('#timeblock-7').val()
     },
     {
         "element": $('#timeblock-8'),
-        "hour": $('#timeblock-8').attr("data-hour")
+        "hour": $('#timeblock-8').attr("data-hour"),
+        "value": $('#timeblock-8').val()
     },
     {
         "element": $('#timeblock-9'),
-        "hour": $('#timeblock-9').attr("data-hour")
+        "hour": $('#timeblock-9').attr("data-hour"),
+        "value": $('#timeblock-9').val()
     }
 ]
 
@@ -64,7 +74,25 @@ function addTimeBlockClasses(){
             timeBlock[i].element.addClass("present")
             console.log(timeBlock[i].hour+" is equal to "+currentHour)
         }
+
+        // Will keep whatever input the user saved into local storage for each timeblock
+        var localCache = localStorage.getItem(timeBlock[i].element.attr("id"))
+        timeBlock[i].innerHTML = localCache
+        console.log(localCache)
     }
 }
 
 addTimeBlockClasses()
+
+// // Below is the code that will add functionality to the save buttons
+$(document).ready(function(){
+    saveBtns.click(function(event){
+        event.preventDefault();
+
+        var inputItem = $(this).siblings("input").val()
+        var itemHour = $(this).siblings("input").attr("id")
+        console.log(inputItem)
+        
+        localStorage.setItem(itemHour, inputItem)
+    })
+})
